db7e65023814974d3edc7e25bc3d7b5964491dd1
pandoragear <andy.dang.it@gmail.com>
2021-08-18 22:53:10 -0500

    jsx convert

--- Concept/index.js
+++ Concept/index.js
@@ -1,12 +1,15 @@
 const reactContentRoot = document.getElementById("root");
 
-const myFirstElement = React.createElement(
-  "ul",
-  null,
-  [
+const myFirstElement = React.createElement("ul", null, [
   React.createElement("li", null, "item1"),
-  React.createElement("li", null, "item2")
-  ]
+  React.createElement("li", null, "item2"),
+]);
+
+const myJSXElement = (
+  <ul>
+    <li>item 1 </li>
+    <li>item 2 </li>
+  </ul>
 );
 
 ReactDOM.render(myFirstElement, reactContentRoot);
