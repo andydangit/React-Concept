da2554e32f08503417458ebe901a1f038ca5cedb
pandoragear <andy.dang.it@gmail.com>
2021-08-18 23:38:57 -0500

    git commit -a -m

--- Concept/index.js
+++ Concept/index.js
@@ -1,9 +1,9 @@
 const reactContentRoot = document.getElementById("root");
 
-const myFirstElement = React.createElement("ul", null, [
-  React.createElement("li", null, "item1"),
-  React.createElement("li", null, "item2"),
-]);
+// const myFirstElement = React.createElement("ul", null, [
+//   React.createElement("li", null, "item1"),
+//   React.createElement("li", null, "item2"),
+// ]);
 
 const myJSXElement = (
   <ul>
@@ -12,4 +12,4 @@ const myJSXElement = (
   </ul>
 );
 
-ReactDOM.render(myFirstElement, reactContentRoot);
+ReactDOM.render(myJSXElement, reactContentRoot);
