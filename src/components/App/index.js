a0287b7a83f1e9adcfdfe248fe4ee3ae0550f5e9
Andy Dang <andy.dang.it@gmail.com>
2021-08-25 21:33:59 -0500

    inital files

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,48 +3,44 @@ import "../App/index.css";
 import CountButton from "../CountButton/index";
 import SearchBar from "../SearchBar";
 
-
 function App() {
-
-const [productsState, setProductsState] = useState([])
-
+  const [productsState, setProductsState] = useState([]);
 
   useEffect(() => {
-
     // this is asynchronous
-    fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(productsArray=>{
-              const newProductsState = productsArray.map((product) => {
-                return product.title
-              })
-              setProductsState(newProductsState)
-            })
-
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((productsArray) => {
+        const newProductsState = productsArray.map((product) => {
+          return product.title;
+        });
+        setProductsState(newProductsState);
+      });
 
     // setTimeout(() => {
     //   setProductsState ([
-    //     "tooth paste", 
-    //     "tooth brush", 
+    //     "tooth paste",
+    //     "tooth brush",
     //     "mouth wash",
     //     "dental floss",
     //     'mouth guard',
     //   ])
     // }, 2000)
+  }, []);
 
-    
-  }, [])
-
-const hasProducts = productsState.length > 0;
+  const hasProducts = productsState.length > 0;
 
   return (
     <div className="App">
-
-    {hasProducts ? <SearchBar products={productsState} />: "Loading"}
-
       
-      
-      {/* <CountButton incrementBy={1} buttonColor={'yellow'} borderRadius={"2px"} />  */}
+      <CountButton
+        incrementBy={1}
+        buttonColor={"yellow"}
+        borderRadius={"2px"}
+      />
+
+      {hasProducts ? <SearchBar products={productsState} /> : "Loading"}
+
       {/* <CountButton incrementBy={5} buttonColor={"red"} borderRadius={"30px"}/> 
       <CountButton incrementBy={7} buttonColor={"green"} borderRadius={"1px"}/>  */}
     </div>
